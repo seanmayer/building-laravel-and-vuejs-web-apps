61f6ee0ecf506cb1e981bd23bffbb5c40b5a2743
Sean Mayer Laptop <m014609f@student.staffs.ac.uk>
2020-05-23 18:30:09 +0100

    MV* Architectures - Integrating a layout - Creating a model and seeding the database - Building views with Blade Templates - Injecting props in Vue components - Building a CRUD component (delete)

--- nadias/routes/web.php
+++ nadias/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,16 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
+Route::get('/', 'HomeController@index')->name('home');
 
+Route::get('/categories', 'CategoryController@index');
